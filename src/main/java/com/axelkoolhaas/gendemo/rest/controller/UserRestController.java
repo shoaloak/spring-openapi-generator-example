ee17a17ec984ded77f96e9b3bf5f88db853246d6
Axel Koolhaas <axel.koolhaas@hotmail.com>
2023-10-28 16:42:06 +0200

    OpenAPI generator doesn't understand multitypes. Commit before switching to manual endpoints.

--- src/main/java/com/axelkoolhaas/gendemo/rest/controller/UserRestController.java
+++ src/main/java/com/axelkoolhaas/gendemo/rest/controller/UserRestController.java
@@ -1,7 +1,13 @@
 package com.axelkoolhaas.gendemo.rest.controller;
 
 import com.axelkoolhaas.gendemo.rest.api.UsersApi;
+import com.axelkoolhaas.gendemo.rest.dto.GetUserById200ResponseDto;
+import com.axelkoolhaas.gendemo.rest.dto.RawDto;
+import com.axelkoolhaas.gendemo.rest.dto.UserDto;
+import org.apache.catalina.User;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
@@ -9,12 +15,29 @@ import org.springframework.stereotype.Controller;
 public class UserRestController implements UsersApi {
 
     @Override
-    public ResponseEntity<Void> getUserById(Integer id) {
-        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    public ResponseEntity<GetUserById200ResponseDto> getUserById(Integer id) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        GetUserById200ResponseDto responseDto;
+        if (id == 1) {
+            responseHeaders.setContentType(MediaType.TEXT_PLAIN);
+
+
+            // Should've been String. OpenAPI generator doesn't support String yet.
+            responseDto = new UserDto();
+            responseHeaders.setContentType(MediaType.APPLICATION_JSON);
+        } else {
+            var user = new UserDto();
+            user.setId(1);
+            user.setFirstName("John");
+
+            responseDto = user;
+            responseHeaders.setContentType(MediaType.APPLICATION_JSON);
+        }
+        return new ResponseEntity<>(responseDto, responseHeaders,HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<Void> getUsers() {
-        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    public ResponseEntity<String> getUsers() {
+        return new ResponseEntity<>("Hello there", HttpStatus.OK);
     }
 }
